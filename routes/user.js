ef262389e210c97d4de233d2281345fad6974e44
Divij Katyal <divijkatyal@gmail.com>
2022-06-20 15:49:52 +0530

    user-route-added

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express=require("express");
+const { read } = require("fs");
 const User =require("../models/user_model");
 const app=express();
 const router=express.Router();
 
-app.route("/register").post((req,res)=> {
+router.route("/register").post((req,res)=> {
     const user=new User({
         username:req.body.username,
         password:req.body.password,
@@ -16,6 +17,29 @@ app.route("/register").post((req,res)=> {
         res.status(403).json({msg:err});
     })
     res.json("Registered");
+});
+
+router.route("/update/:username").patch((req,res)=> {
+    User.findOneAndUpdate({username:req.params.username},{$set:{password:req.body.password}},(err,result)=>{
+        if(err) return res.status(500).json({msg:err});
+        const msg={
+            msg:"Password updated Successfully",
+            username:req.params.username
+        }
+        return read.json(msg);
+    });
+});
+
+
+router.route("/delete/:username").delete((req,res)=> {
+    User.findOneAndDelete({username:req.params.username},(err,result)=>{
+        if(err) return res.status(500).json({msg:err});
+        const msg={
+            msg:"Deleted Successfully",
+            username:req.params.username
+        }
+        return read.json(msg);
+    });
 });
 
 module.exports=router;
